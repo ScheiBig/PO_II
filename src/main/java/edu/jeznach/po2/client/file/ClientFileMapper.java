a90259d0036c60ec7122edec3d9dda52e6b0ea69
Marcin Jeznach <Scheissig@gmail.com>
2020-02-09 20:48:37 +0100

    Implement ClientMapping file manipulation methods

--- src/main/java/edu/jeznach/po2/client/file/ClientFileMapper.java
+++ src/main/java/edu/jeznach/po2/client/file/ClientFileMapper.java
@@ -3,6 +3,8 @@ package edu.jeznach.po2.client.file;
 import edu.jeznach.po2.client.file.ClientMapping.Directories;
 import edu.jeznach.po2.client.gui.NotificationSender;
 import edu.jeznach.po2.common.file.FileMapper;
+import edu.jeznach.po2.common.file.FileMapping;
+import edu.jeznach.po2.common.file.SharedFileMapping;
 import edu.jeznach.po2.common.log.Log;
 import edu.jeznach.po2.common.util.Pair;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +15,7 @@ import org.yaml.snakeyaml.error.YAMLException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 /**
  * Represents mapping of client directories structure.
@@ -29,25 +32,181 @@ public class ClientFileMapper extends FileMapper<ClientMapping> {
      * @param mapping the mapping object
      * @param file    the file that is used to store mapping
      */
-    public ClientFileMapper(@NotNull ClientMapping mapping, @Nullable File file) {
-        super(mapping, file);
-    }
+    public ClientFileMapper(@NotNull ClientMapping mapping, @Nullable File file) { super(mapping, file); }
 
     @Override
     public boolean attachFile(@NotNull File file, @NotNull String checksum, @NotNull String node) {
         checkNode(node);
+        String relativeFilePath = getRelativePath(file,
+                                                  new File(getMapping().getClient_location()
+                                                           + File.separator + node));
+        if (Directories.files.toString().equals(node)) {
+            if (getMapping().getFiles() == null) getMapping().setFiles(new ArrayList<>());
+            Optional<ClientFileMapping> fileOptional = getMapping().getFiles()
+                                                                   .stream()
+                                                                   .filter(f -> f.getPathname().equals(relativeFilePath))
+                                                                   .findFirst();
+            if (!fileOptional.isPresent()) {
+                getMapping().getFiles()
+                            .add(new ClientFileMapping(
+                                    relativeFilePath, file.length(), checksum, file.lastModified()
+                            ));
+                mappingFile.ifPresent(this::dumpToFile);
+                return true;
+            }
+        } else if (Directories.cancel.toString().equals(node)) {
+            if (getMapping().getCancelled_files() == null) getMapping().setCancelled_files(new ArrayList<>());
+            Optional<FileMapping> fileOptional = getMapping().getCancelled_files()
+                                                             .stream()
+                                                             .filter(f -> f.getPathname().equals(relativeFilePath))
+                                                             .findFirst();
+            if (!fileOptional.isPresent()) {
+                getMapping().getCancelled_files()
+                            .add(new FileMapping(
+                                    relativeFilePath, file.length(), checksum, file.lastModified()
+                            ));
+                mappingFile.ifPresent(this::dumpToFile);
+                return true;
+            }
+        } else if (Directories.shared.toString().equals(node)) {
+            String[] path = relativeFilePath.split(Pattern.quote(File.separator), 2);
+            if (path.length != 2) throw new IllegalArgumentException("Path " + relativeFilePath + " is not a shared file path.");
+            if (getMapping().getShared_files() == null) getMapping().setShared_files(new ArrayList<>());
+            Optional<SharedFileMapping> fileOptional = getMapping().getShared_files()
+                                                                   .stream()
+                                                                   .filter(f -> f.getPathname().equals(path[1]))
+                                                                   .filter(f -> f.getOwner().equals(path[0]))
+                                                                   .findFirst();
+            if (!fileOptional.isPresent()) {
+                getMapping().getShared_files()
+                            .add(new SharedFileMapping(
+                                    path[1], file.length(), checksum, file.lastModified(), path[0]
+                            ));
+                mappingFile.ifPresent(this::dumpToFile);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean detachFile(@NotNull File file, @NotNull String node) {
         checkNode(node);
+        String relativeFilePath = getRelativePath(file,
+                                                  new File(getMapping().getClient_location()
+                                                           + File.separator + node));
+        if (Directories.files.toString().equals(node)) {
+            if (getMapping().getFiles() != null) {
+                Optional<ClientFileMapping> fileOptional = getMapping().getFiles()
+                                                                       .stream()
+                                                                       .filter(f -> f.getPathname().equals(relativeFilePath))
+                                                                       .findFirst();
+                if (fileOptional.isPresent()) {
+                    getMapping().getFiles().remove(fileOptional.get());
+                    mappingFile.ifPresent(this::dumpToFile);
+                    return true;
+                }
+            }
+        } else if (Directories.cancel.toString().equals(node)) {
+            if (getMapping().getCancelled_files() != null) {
+                Optional<FileMapping> fileOptional = getMapping().getCancelled_files()
+                                                                 .stream()
+                                                                 .filter(f -> f.getPathname().equals(relativeFilePath))
+                                                                 .findFirst();
+                if (fileOptional.isPresent()) {
+                    getMapping().getCancelled_files().remove(fileOptional.get());
+                    mappingFile.ifPresent(this::dumpToFile);
+                    return true;
+                }
+            }
+        } else if (Directories.shared.toString().equals(node)) {
+            if (getMapping().getShared_files() != null) {
+                String[] path = relativeFilePath.split(Pattern.quote(File.separator), 2);
+                if (path.length != 2)
+                    throw new IllegalArgumentException("Path " + relativeFilePath + " is not a shared file path.");
+                Optional<SharedFileMapping> fileOptional = getMapping().getShared_files()
+                                                                       .stream()
+                                                                       .filter(f -> f.getPathname().equals(path[1]))
+                                                                       .filter(f -> f.getOwner().equals(path[0]))
+                                                                       .findFirst();
+                if (fileOptional.isPresent()) {
+                    getMapping().getShared_files().remove(fileOptional.get());
+                    mappingFile.ifPresent(this::dumpToFile);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public boolean updateFile(@NotNull File file, @NotNull String checksum, @NotNull String node) {
         checkNode(node);
+        String relativeFilePath = getRelativePath(file,
+                                                  new File(getMapping().getClient_location()
+                                                           + File.separator + node));
+        if (Directories.files.toString().equals(node)) {
+            if (getMapping().getFiles() != null) {
+                Optional<ClientFileMapping> fileOptional = getMapping().getFiles()
+                                                                       .stream()
+                                                                       .filter(f -> f.getPathname().equals(relativeFilePath))
+                                                                       .findFirst();
+                if (fileOptional.isPresent()) {
+                    ClientFileMapping fileMapping = fileOptional.get();
+                    long oldSize = fileMapping.getSize_bytes();
+                    fileMapping.setChecksum(checksum);
+                    fileMapping.setModification_timestamp(file.lastModified());
+                    fileMapping.setSize_bytes(file.length());
+                    getMapping().setUsed_space_bytes(
+                            getMapping().getUsed_space_bytes() - oldSize + file.length()
+                    );
+                    mappingFile.ifPresent(this::dumpToFile);
+                    return true;
+                }
+            }
+        } else if (Directories.cancel.toString().equals(node)) {
+            if (getMapping().getCancelled_files() != null) {
+                Optional<FileMapping> fileOptional = getMapping().getCancelled_files()
+                                                                 .stream()
+                                                                 .filter(f -> f.getPathname().equals(relativeFilePath))
+                                                                 .findFirst();
+                if (fileOptional.isPresent()) {
+                    FileMapping fileMapping = fileOptional.get();
+                    long oldSize = fileMapping.getSize_bytes();
+                    fileMapping.setChecksum(checksum);
+                    fileMapping.setModification_timestamp(file.lastModified());
+                    fileMapping.setSize_bytes(file.length());
+                    getMapping().setUsed_space_bytes(
+                            getMapping().getUsed_space_bytes() - oldSize + file.length()
+                    );
+                    mappingFile.ifPresent(this::dumpToFile);
+                    return true;
+                }
+            }
+        } else if (Directories.shared.toString().equals(node)) {
+            if (getMapping().getShared_files() != null) {
+                String[] path = relativeFilePath.split(Pattern.quote(File.separator), 2);
+                if (path.length != 2)
+                    throw new IllegalArgumentException("Path " + relativeFilePath + " is not a shared file path.");
+                Optional<SharedFileMapping> fileOptional = getMapping().getShared_files()
+                                                                       .stream()
+                                                                       .filter(f -> f.getPathname().equals(path[1]))
+                                                                       .filter(f -> f.getOwner().equals(path[0]))
+                                                                       .findFirst();
+                if (fileOptional.isPresent()) {
+                    FileMapping fileMapping = fileOptional.get();
+                    long oldSize = fileMapping.getSize_bytes();
+                    fileMapping.setChecksum(checksum);
+                    fileMapping.setModification_timestamp(file.lastModified());
+                    fileMapping.setSize_bytes(file.length());
+                    getMapping().setUsed_space_bytes(
+                            getMapping().getUsed_space_bytes() - oldSize + file.length()
+                    );
+                    mappingFile.ifPresent(this::dumpToFile);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
